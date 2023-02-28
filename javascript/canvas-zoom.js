3c0dec8df137029289a0e43568a6b32b18b4c208
richrobber2 <richardrgguno@hotmail.com>
2023-02-28 01:35:12 -0800

    adding context menu support
    I plan on making this work for more things in the future

--- javascript/canvas-zoom.js
+++ javascript/canvas-zoom.js
@@ -1,32 +1,176 @@
-// uses timeout because it does not load when it does not have this and i cant seem to use onload events probably because most things seem to be handled by javascript
+// uses timeout because it other on dom events dont work (havent tested them all through)
 setTimeout(function () {
-
-  // Select the element
+  // Main variables
   const element = document.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg");
-
-  // Set initial values for zoom level and panning
+  element.style.zIndex = "9999";
+  const button = document.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg > div.h-60.bg-gray-200 > div > div.z-50.top-2.right-2.justify-end.flex.gap-1.absolute > button:nth-child(1)");
+  let activeMenu = null;
   let zoomLevel = 1;
   let panX = 0;
   let panY = 0;
 
-  // Add event listener for mousewheel
+
+
+  // Helper Functions
+  function getHoveredElementWorkAround(event) {
+    const [targetElement] = event.composedPath().filter(element => !element.shadowRoot);
+    return targetElement;
+  }
+  function createContextMenu(options, event) {
+    // Remove the active menu if it exists
+    if (activeMenu) {
+      activeMenu.remove();
+    }
+
+    const menu = document.createElement("div");
+    menu.classList.add("context-menu");
+
+    // Loop through the options and create a menu item for each one
+    options.forEach((option) => {
+      const menuItem = document.createElement("div");
+      menuItem.innerText = option.label;
+      menuItem.addEventListener("click", () => {
+        option.callback(event);
+        menu.remove();
+      });
+      menu.appendChild(menuItem);
+    });
+
+    // Position the menu at the mouse pointer
+    document.body.appendChild(menu);
+    menu.style.left = `${event.pageX}px`;
+    menu.style.top = `${event.pageY}px`;
+    menu.style.position = "absolute";
+
+    // Listen for clicks outside the menu and close it if necessary
+    const closeMenu = () => {
+      menu.remove();
+      document.removeEventListener("click", closeMenu);
+    };
+    document.addEventListener("click", closeMenu);
+
+    // Set the active menu to the new menu
+    activeMenu = menu;
+  }
+  function getContextMenuOptions(element) {
+    const classNames = Array.from(element.classList); // Get an array of all the class names on the element
+    const nodeName = element.nodeName || null
+    const id = element.id || null;
+
+
+
+    const optionsByClass = {
+      link: [
+        {
+          label: "Open Link",
+          callback: () => {
+            // Code to open the link
+          },
+        },
+        {
+          label: "Copy Link Address",
+          callback: () => {
+            // Code to copy the link address
+          },
+        },
+      ],
+      image: [
+        {
+          label: "Save Image",
+          callback: () => {
+            // Code to save the image
+          },
+        },
+        {
+          label: "Copy Image Address",
+          callback: () => {
+            // Code to copy the image address
+          },
+        },
+      ],
+    };
+    const optionsById = {
+      txt2img_sampling: [
+        {
+          label: "this is just a test to see if this works",
+          callback: () => {
+            // Code to handle Option 1
+          },
+        },
+        {
+          label: "Option 2",
+          callback: () => {
+            // Code to handle Option 2
+          },
+        },
+      ],
+    };
+    const optionsBynodeName = {
+      SELECT: [
+        {
+          label: "copy value",
+          callback: () => {
+            const valueToCopy = element.selectedOptions[0].value;
+            navigator.clipboard.writeText(valueToCopy)
+              .then(() => {
+                console.log('Copied to clipboard:', valueToCopy);
+              })
+              .catch((error) => {
+                console.error('Failed to copy to clipboard:', error);
+              });
+          },
+        }
+      ],
+      INPUT: [
+        {
+          label: "copy value",
+          callback: () => {
+            const valueToCopy = element.value;
+            navigator.clipboard.writeText(valueToCopy)
+              .then(() => {
+                console.log('Copied to clipboard:', valueToCopy);
+              })
+              .catch((error) => {
+                console.error('Failed to copy to clipboard:', error);
+              });
+          },
+        }
+      ],
+    };
+    const defaultOptions = [
+      {
+        label: "Copy",
+        callback: () => {
+          // Code to copy the text
+        },
+      },
+    ];
+
+
+
+    let options = defaultOptions;
+    classNames.forEach((name) => {
+      if (optionsByClass[name]) {
+        options = options.concat(optionsByClass[name]);
+      }
+    });
+    if (optionsBynodeName[nodeName]) {
+      options = options.concat(optionsBynodeName[nodeName]);
+    }
+    if (optionsById[id]) {
+      options = options.concat(optionsById[id]);
+    }
+    return options;
+  }
+
+
+  // Main listeners
   element.addEventListener("wheel", (e) => {
-    // Check if Shift key is held down
     if (e.shiftKey) {
-      // Prevent default scrolling behavior
       e.preventDefault();
-
-      // Calculate new zoom level
       const newZoomLevel = e.deltaY > 0 ? zoomLevel * 0.9 : zoomLevel * 1.1;
-
-      // Limit zoom level to between 1 and 10
       zoomLevel = Math.max(1, Math.min(newZoomLevel, 10));
-
-      // Update element style with new zoom level
       element.style.transform = `scale(${zoomLevel}) translate(${panX}px, ${panY}px)`;
-
-      // Make element above all other elements
-      element.style.zIndex = "9999";
     }
     if (e.ctrlKey) {
 
@@ -49,25 +193,6 @@ setTimeout(function () {
     }
 
   });
-
-  // Add event listener for keydown
-  document.addEventListener("keydown", (e) => {
-    // Check if Shift key is held down
-    if (e.shiftKey) {
-      // Check if R key is pressed
-      if (e.key === "r") {
-        // Reset zoom level and panning
-        zoomLevel = 1;
-        panX = 0;
-        panY = 0;
-
-        // Update element style with new zoom level and panning
-        element.style.transform = `scale(${zoomLevel}) translate(${panX}px, ${panY}px)`;
-      }
-    }
-  });
-
-  // Add event listener for mousedown
   element.addEventListener("mousedown", (e) => {
     // Check if Shift key is held down
     if (e.shiftKey) {
@@ -77,16 +202,9 @@ setTimeout(function () {
       const startPanX = panX;
       const startPanY = panY;
 
-      // Add event listener for mousemove
       document.addEventListener("mousemove", handleMouseMove);
-
-      // Add event listener for mouseup
       document.addEventListener("mouseup", handleMouseUp);
-
-      // Prevent default dragging behavior
       e.preventDefault();
-
-      // Define function for handling mousemove
       function handleMouseMove(e) {
         // Calculate new panning values
         const deltaX = e.clientX - startX;
@@ -99,10 +217,6 @@ setTimeout(function () {
         element.style.pointerEvents = "none";
       }
 
-      // Get the button element and store it in a variable
-      const button = document.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg > div.h-60.bg-gray-200 > div > div.z-50.top-2.right-2.justify-end.flex.gap-1.absolute > button:nth-child(1)");
-
-      // Define function for handling mouseup
       function handleMouseUp() {
         // Create and cache a new mouse event
         const event = new MouseEvent('click', {
@@ -121,7 +235,15 @@ setTimeout(function () {
         document.removeEventListener("mousemove", handleMouseMove);
         document.removeEventListener("mouseup", handleMouseUp);
       }
-
     }
   });
+  document.querySelector("body > gradio-app").shadowRoot.addEventListener("contextmenu", (event) => {
+    event.preventDefault(); // Prevent the browser's default context menu from appearing
+    const hoveredElement = getHoveredElementWorkAround(event); // Get the hovered element using the provided function
+    const options = getContextMenuOptions(hoveredElement);
+    createContextMenu(options, event, getHoveredElementWorkAround(event));
+  });
+
+
+
 }, 3000);
