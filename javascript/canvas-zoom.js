12fd3ab75fe462fad5cdce98888d672328948a68
richrobber2 <richardrgguno@hotmail.com>
2023-03-24 01:23:09 -0700

    im struggling to make this work correctly

--- javascript/canvas-zoom.js
+++ javascript/canvas-zoom.js
@@ -1,125 +1,63 @@
 // uses timeout because it does not load when it does not have this and i cant seem to use onload events probably because most things seem to be handled by javascript
 setTimeout(function () {
-
-  // Select the element
   const element = document.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg");
-
-
-  // Add event listener for mousewheel
+  let [zoomLevel, panX, panY] = [1, 0, 0];
   element.addEventListener("wheel", (e) => {
-    // Check if Shift key is held down
     if (e.shiftKey) {
-      // Prevent default scrolling behavior
       e.preventDefault();
 
-      // Calculate new zoom level
-      const newZoomLevel = e.deltaY > 0 ? zoomLevel * 0.9 : zoomLevel * 1.1;
+      // handle zooming here panning will be handled by the mousemove event
+      const startZoomLevel = zoomLevel;
 
-      // Limit zoom level to between 1 and 10
-      zoomLevel = Math.max(1, Math.min(newZoomLevel, 10));
+      // Calculate new zoom level + or - 0.1 based on direction of scroll amount should change based on zoom level if above 5 then 0.5 if below 5 then 0.1
+      const delta = zoomLevel > 3 ? 0.5 : 0.1;
+      zoomLevel = e.deltaY > 0 ? startZoomLevel - delta : startZoomLevel + delta;
 
-      // Update element style with new zoom level
+      // max and min zoom level using clamp min is 0.5 and max is 10
+      zoomLevel = Math.min(Math.max(0.5, zoomLevel), 10);
+
+      // update the zoom level
       element.style.transform = `scale(${zoomLevel}) translate(${panX}px, ${panY}px)`;
-
-      // Make element above all other elements
-      element.style.zIndex = "9999";
     }
     if (e.ctrlKey) {
-
-      // Prevent default behavior of the event if Ctrl key is held down
       e.preventDefault();
-      // Get the input element and its value
-      const input = document.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg > div.h-60.bg-gray-200 > div > div.z-50.top-10.right-2.justify-end.flex.gap-1.absolute > span > input");
-      let value = parseFloat(input.value);
 
-      // Determine scroll direction and update input value accordingly
-      if (e.deltaY > 0) {
-        value -= 3;
-      } else {
-        value += 3;
+      const input = document.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg > div.h-60.bg-gray-200 > div > div.z-50.top-10.right-2.justify-end.flex.gap-1.absolute > span > input")
+      if (input == null) {
+        document.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg > div.h-60.bg-gray-200 > div > div.z-50.top-10.right-2.justify-end.flex.gap-1.absolute > span > button").click();
       }
-      // Set the new input value
+      let value = parseFloat(input.value);
+      value += e.deltaY > 0 ? -3 : 3;
       input.value = value;
       const changeEvent = new Event('change');
       input.dispatchEvent(changeEvent);
     }
-
   });
 
-  // Add event listener for keydown
-  document.addEventListener("keydown", (e) => {
-    // Check if Shift key is held down and R key is pressed
-    if (e.shiftKey && e.key === "r") {
-      // Reset zoom level and panning
-      zoomLevel = 1;
-      panX = 0;
-      panY = 0;
-
-      // Update element style with new zoom level and panning
-      element.style.transform = `scale(${zoomLevel}) translate(${panX}px, ${panY}px)`;
-    }
-  });
-
-  // Set initial values for zoom level and panning
-  let zoomLevel = 1;
-  let panX = 0;
-  let panY = 0;
-
-  // Cache the document object and the button element
-  const doc = document;
-  const button = doc.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg > div.h-60.bg-gray-200 > div > div.z-50.top-2.right-2.justify-end.flex.gap-1.absolute > button:nth-child(1)");
-
-  // Define function for handling mousemove
-  function handleMouseMove(e) {
-    // Calculate new panning values
-    const deltaX = e.clientX - startX;
-    const deltaY = e.clientY - startY;
-    panX = startPanX + deltaX;
-    panY = startPanY + deltaY;
-
-    // Update element style with new panning values and disable pointer events
+  function handleMove(e) {
+    e.preventDefault();
+    panX += e.movementX;
+    panY += e.movementY;
     element.style.transform = `scale(${zoomLevel}) translate(${panX}px, ${panY}px)`;
     element.style.pointerEvents = "none";
   }
 
-  // Define function for handling mouseup
-  function handleMouseUp() {
-    // Create and cache a new mouse event
-    const event = new MouseEvent('click', {
-      view: window,
-      bubbles: true,
-      cancelable: true
-    });
-
-    // Dispatch the click event using the cached button element
-    button.dispatchEvent(event);
-
-    // Set pointer events back to their original value
-    element.style.pointerEvents = "auto";
-
-    // Remove event listeners for mousemove and mouseup
-    doc.removeEventListener("mousemove", handleMouseMove);
-    doc.removeEventListener("mouseup", handleMouseUp);
+  function handleEnd() {
+    document.removeEventListener("mousemove", handleMove);
+    document.removeEventListener("mouseup", handleEnd);
+    element.style.pointerEvents = "auto"; 
+  }
+  function handleundo() {
+    document.removeEventListener("mouseleave", handleundo);
+    document.querySelector("body > gradio-app").shadowRoot.querySelector("#img2maskimg > div.h-60.bg-gray-200 > div > div.z-50.top-2.right-2.justify-end.flex.gap-1.absolute > button:nth-child(1)").click();
   }
 
-  // Add event listener for mousedown
   element.addEventListener("mousedown", (e) => {
-    // Check if Shift key is held down
     if (e.shiftKey) {
-      // Set initial values for mouse position and panning
-      const startX = e.clientX;
-      const startY = e.clientY;
-      const startPanX = panX;
-      const startPanY = panY;
-
-      // Add event listener for mousemove
-      doc.addEventListener("mousemove", handleMouseMove);
-
-      // Add event listener for mouseup
-      doc.addEventListener("mouseup", handleMouseUp);
-
-      // Prevent default dragging behavior
       e.preventDefault();
+      document.addEventListener("mousemove", handleMove);
+      document.addEventListener("mouseup", handleEnd);
+      element.addEventListener("mouseleave", handleundo);
     }
   });
 }, 3000);
