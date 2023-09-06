efba3a949db12dfa9b2c3d214e8e0b8bca2b3376
Danil Boldyrev <daswerq123@gmail.com>
2023-09-06 06:11:55 +0300

    Improving proper uninstallation
    Now if you disable extesion or disable all of them, gradio will automaticly reinstall

--- detect_extension.py
+++ detect_extension.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 import launch
 
 # Path to Python executable
@@ -13,12 +14,26 @@ script_path = os.path.realpath(__file__)
 
 # Path to the 'requirements.txt' file
 requirements_path = os.path.join(sys.path[2], "requirements.txt")
+config_path = os.path.join(sys.path[2], "config.json")
 
 # Read the version of 'gradio' from 'requirements.txt'
-with open(requirements_path, "r") as file:
-    lines = file.readlines()
-    gradio_line = next((line for line in lines if line.startswith("gradio")), None)
-    gradio_version = gradio_line.split("==")[1].strip() if gradio_line else None
+if os.path.isfile(requirements_path):
+    with open(requirements_path, "r") as file:
+        lines = file.readlines()
+        gradio_line = next((line for line in lines if line.startswith("gradio")), None)
+        gradio_version = gradio_line.split("==")[1].strip() if gradio_line else None
+
+# Check if config.json exists
+if os.path.isfile(config_path):
+    with open(config_path, 'r') as config_file:
+        config = json.load(config_file)
+        # Check if "canvas-zoom" is in the list of disabled_extensions
+        if "canvas-zoom" in config.get('disabled_extensions', []):
+            launch.run(f'"{python}" -m pip install --force-reinstall --no-deps gradio=={gradio_version}', desc=f"Uninstalling modified gradio for canvas-zoom", errdesc=f"Couldn't uninstall canvas-zoom", live=False)
+
+        # Check if disable_all_extensions is not "none"
+        if config.get('disable_all_extensions', 'none') == 'all':
+            launch.run(f'"{python}" -m pip install --force-reinstall --no-deps gradio=={gradio_version}', desc=f"Uninstalling modified gradio for canvas-zoom", errdesc=f"Couldn't uninstall canvas-zoom", live=False)
 
 # Check if the folder exists
 if not os.path.exists(canvasZoomPath) and gradio_version is not None:
